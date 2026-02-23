281b83ec582a150bfdff63d4721858223e7391c8
AmieF88 <amiemfreeman88@gmail.com>
2026-02-22 18:28:13 -0600

    day 5

--- restaurant/src/App.jsx
+++ restaurant/src/App.jsx
@@ -3,21 +3,24 @@ import "./App.css";
 import Menu from "./menu.jsx";
 import OrderPage from "./orderPage.jsx";
 import Chatbot from "./Chatbot";
+import Home from "./home.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <nav style={{ display: "flex", gap: "15px", padding: "15px" }}>
-        <Link to="/menu">Menu</Link>
+        <Link to="/"><button>Home</button></Link>
+        <Link to="/menu"><button>Menu</button></Link>
         <Link to="/order">
           <button>Order Now</button>
         </Link>
         <Link to="/chat"><button>Chat</button>
         </Link>
+
       </nav>
 
       <Routes>
-        <Route path="/" element={<Menu />} />
+        <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/order" element={<OrderPage />} />
         <Route path="/chat" element={<Chatbot />} />
---
+++ restaurant/src/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Welcome to Our Restaurant</h1>
+      <p>Explore our menu, place an order, or chat with our AI waiter!</p>
+    </div>
+  );
+}
